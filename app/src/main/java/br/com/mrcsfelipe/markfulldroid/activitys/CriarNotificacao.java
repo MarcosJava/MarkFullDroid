7451cfbc27bfffc14ef5a42fd61fb90edb6791e4
Marcos Felipe Souza <mfelipesp@gmail.com>
2015-10-14 02:07:35 -0300

    Ultimo passo funcionando POST, GET e DELETE

--- app/src/main/java/br/com/mrcsfelipe/markfulldroid/activitys/CriarNotificacao.java
+++ app/src/main/java/br/com/mrcsfelipe/markfulldroid/activitys/CriarNotificacao.java
@@ -60,7 +60,6 @@ public class CriarNotificacao extends Service {
 
     private class VerificacaoSend implements Runnable{
 
-        List<Person> persons = new ArrayList<>();
 
         @Override
         public void run() {
@@ -72,97 +71,12 @@ public class CriarNotificacao extends Service {
 
             try {
 
-                enviarTodos();
-
             }catch (Exception e){
                 Log.e(getPackageName(), e.getMessage(), e);
             }
 
         }
 
-
-        public void enviarTodos(){
-            //persons = Person.findWithQuery(Person.class, "Select * from Person where foiEnviado = ?", "false");
-            //new HttpRequestTask().execute(MediaType.APPLICATION_JSON);
-
-        }
-
-        private class HttpRequestTask extends AsyncTask<MediaType, Void, String> {
-
-            @Override
-            protected String doInBackground(MediaType... params) {
-                try {
-
-                    MediaType mediaType = params[0];
-
-                    HttpHeaders requestHeaders = new HttpHeaders();
-
-                    // Sending a JSON or XML object i.e. "application/json" or "application/xml"
-                    requestHeaders.setContentType(mediaType);
-
-                    // Populate the Message object to serialize and headers in an
-                    // HttpEntity object to use for the request
-                    PersonsModel person = new PersonsModel();
-                    person.setPersons(persons);
-                    HttpEntity<PersonsModel> requestEntity = new HttpEntity<PersonsModel>(person, requestHeaders);
-
-                    RestTemplate restTemplate = new RestTemplate();
-
-                    restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-                    if (mediaType == MediaType.APPLICATION_JSON) {
-                        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-                    } else if (mediaType == MediaType.APPLICATION_XML) {
-                        restTemplate.getMessageConverters().add(new SimpleXmlHttpMessageConverter());
-                    }
-
-
-                    ResponseEntity<String> response =
-                            restTemplate.postForEntity(SalvarActivity.url, requestEntity, String.class);
-
-                    Log.i("POST", response.getBody().toString());
-
-                    if(response.getStatusCode().value() == 400){
-                        return "";
-                    } else if (response.getStatusCode().value() == 500){
-                        return "erroServer";
-                    } else if (response.getStatusCode().value() == 404) {
-                        return "serverOff";
-                    }
-
-                    // Return the response body to display to the user
-                    return response.getBody();
-
-
-
-                } catch (Exception e) {
-                    return "erroServer";
-                }
-            }
-
-            @Override
-            protected void onPostExecute(String person) {
-
-                if(person == ""){
-
-                }else if(person == "erroServer"){
-
-
-                }else if(person == "serverOff"){
-
-
-                }else {
-                    enviadoSalvaSQL();
-                    Log.d("POST", person.toString());
-                }
-            }
-
-            public void enviadoSalvaSQL(){
-                //Person p = Person.findById(Person.class, person.getId());
-                //p.setFoiEnviado("true");
-                //p.save();
-            }
-
-        }
     }
 
 
--- app/src/main/java/br/com/mrcsfelipe/markfulldroid/activitys/MainActivity.java
+++ app/src/main/java/br/com/mrcsfelipe/markfulldroid/activitys/MainActivity.java
@@ -1,18 +1,21 @@
 package br.com.mrcsfelipe.markfulldroid.activitys;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,14 +30,21 @@ import br.com.mrcsfelipe.markfulldroid.R;
 import br.com.mrcsfelipe.markfulldroid.model.Person;
 import br.com.mrcsfelipe.markfulldroid.model.Persons;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ListActivity implements AdapterView.OnItemClickListener,
+        DialogInterface.OnClickListener{
 
     private List<Map<String, Object>> personsMAP = new ArrayList<>();
     private Persons persons;
     private SimpleAdapter adapter;
     static final String url = "http://10.0.2.2:8080/MarkFullOne/rest/persons";
+    static final String url_delete = "http://10.0.2.2:8080/MarkFullOne/rest/persons/delete";
+    static final MediaType mediaType = MediaType.APPLICATION_JSON;
 
 
+    private AlertDialog alertDialog;
+    private AlertDialog dialogConfirmacao;
+    private int posicaoClickPerson = 0; // POSICAO NA LISTA
+    private Integer personDeleted = 0; // ID DO DELETE
 
 
 
@@ -45,6 +55,10 @@ public class MainActivity extends ListActivity {
         //new HttpRequestTask().execute();
         popularAdpter();
         adapter.areAllItemsEnabled();
+        getListView().setOnItemClickListener(this);
+
+        this.alertDialog = criaAlertDialog();
+        this.dialogConfirmacao = criaDialogConfirmacao();
 
     }
 
@@ -72,6 +86,8 @@ public class MainActivity extends ListActivity {
 
 
     public List<Map<String, Object>> getPersons(){
+
+        personsMAP = new ArrayList<>();
 
         Integer size = persons.getPersons().size();
         Log.i("SIZE", size.toString());
@@ -103,7 +119,7 @@ public class MainActivity extends ListActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            new HttpRequestTask().execute();
+            new GETPerson().execute();
             return true;
         } else if (id == R.id.sair) {
             super.onDestroy();
@@ -120,14 +136,102 @@ public class MainActivity extends ListActivity {
     private void notificacao() {
 
     }
-    private class HttpRequestTask extends AsyncTask<Void, Void, Persons> {
+
+
+
+
+
+    /*
+    *
+    *
+    * Click DIALOG MENU
+    *
+    * */
+    private AlertDialog criaAlertDialog() {
+        final CharSequence[] items = {
+                getString(R.string.excluir)};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.opcoes);
+        builder.setItems(items, this);
+        return builder.create();
+    }
+
+
+    /**
+     *
+     *  IMPLEMENTACAO DA INTERFACE DE CLICK NO ITEM
+     *
+     * */
+    @Override
+    public void onItemClick(AdapterView<?> parent,
+                            View view, int position, long id) {
+
+        Map<String, Object> map = personsMAP.get(position);
+
+        String destino = (String) map.get("identify").toString();
+        String mensagem = "Person seleciondo : "+ destino;
+        personDeleted = (Integer) map.get("identify");
+        Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show();
+        this.posicaoClickPerson = position;
+        alertDialog.show();
+    }
+    /**
+     *
+     * DIALOG CONFIRMACAO
+     *
+     */
+
+    private AlertDialog criaDialogConfirmacao() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.confirmacao_exclusao_viagem);
+        builder.setPositiveButton(getString(R.string.sim), this);
+        builder.setNegativeButton(getString(R.string.nao), this);
+        return builder.create();
+    }
+
+
+/******
+ *
+ *  Resposta de todos os dialogs
+ *
+ * ******/
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
+        Log.i("valor On Click no Which", "" + which);
+
+        switch (which){
+            case 0:
+                dialogConfirmacao.show();
+                getListView().invalidateViews();
+                break;
+            case DialogInterface.BUTTON_POSITIVE:
+                personsMAP.remove(this.posicaoClickPerson);
+                new DELETEPerson().execute();
+                getListView().invalidateViews();
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                dialogConfirmacao.dismiss();
+                break;
+
+        }
+    }
+
+    /***
+     *
+     *
+     *  FAZENDO O GET PARA O SERVER
+     *
+     * ***/
+    private class GETPerson extends AsyncTask<Void, Void, Persons> {
         @Override
         protected Persons doInBackground(Void... params) {
             try {
 
                 RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-                Persons person = restTemplate.getForObject(url, Persons.class);
+                Persons person = new Persons();
+                person = restTemplate.getForObject(url, Persons.class);
                 return person;
             } catch (Exception e) {
                 Log.e("MainActivity", e.getMessage(), e);
@@ -138,21 +242,77 @@ public class MainActivity extends ListActivity {
 
         @Override
         protected void onPostExecute(Persons person) {
+            persons = new Persons();
             persons = person;
             for(Person p : persons.getPersons()) {
                 Log.i("REST", p.toString());
             }
 
-            teste();
+            populandoListView();
         }
 
     }
 
 
-    public void teste(){
+    /******
+     *
+     *
+     *  DELETE PARA O SERVIDOR
+     *
+     * *******/
+    private class DELETEPerson extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+
+                //HttpHeaders headers = new HttpHeaders();
+                //headers.set("Accept", String.valueOf(MediaType.APPLICATION_JSON));
+
+
+
+                //HttpEntity<?> entity = new HttpEntity<>(headers);
+                Log.i("Person deleted", personDeleted + "");
+                Map<String, Object> item = new HashMap<String, Object>();
+                item.put("id", personDeleted);
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                restTemplate.delete(url_delete + "/?id=" + personDeleted);
+
+                return "Deletado Com Sucesso";
+
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String person) {
+
+            String mensagem =  getString(R.string.erro_deletar);
+
+            if("Deletado Com Sucesso".equalsIgnoreCase(person.trim())){
+                posicaoClickPerson = 0;
+            } else {
+                Toast toast = Toast.makeText(MainActivity.this, mensagem, Toast.LENGTH_SHORT);
+                toast.show();
+                populandoListView();
+            }
+        }
+
+    }
+
+
+    public void populandoListView(){
+
+        adapter.notifyDataSetChanged();
+
+
         String[] de = {"identify", "nome", "nascimento"};
         int[] para = {R.id.tv_identify, R.id.tv_nome, R.id.tv_dt_nascimento};
-
+        adapter = new SimpleAdapter(this, null, R.layout.activity_main, de, para); // limpando
         adapter = new SimpleAdapter(this, getPersons(), R.layout.activity_main, de, para);
         setListAdapter(adapter);
 
